2b8e94915e78c0877cc03f3d36fbf4d0e59b7ff9
Devi Priyanka <devi26priya@gmail.com>
2016-08-09 18:18:09 +0530

    Update number.java

--- number.java
+++ number.java
@@ -5,10 +5,13 @@ class number
  public static void main(String[] args)
  {
   Scanner s=new Scanner(System.in);
+  if(num>0){
   int num=s.nextInt();
     if(num%2==0)
      System.out.println("even");
   else 
      System.out.println("odd");
-    }
+    }}
+   else
+    System.out.println("0")
     }
