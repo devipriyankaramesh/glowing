aecf654d4b89d72a6fee8690580b367bc8745d5a
Devi Priyanka <devi26priya@gmail.com>
2016-08-09 19:01:45 +0530

    Create removevowels.java

---
+++ removevowels.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class vowels
+{
+ public static void main(String[] args)
+ {
+  Scanner s=new Scanner(System.in);
+  String str=s.next();
+  String s1=str.reverse();
+  String[] a=s1.split[AEIOU,aeiou];
+  System.out.println(a);
+}
+}
+  
