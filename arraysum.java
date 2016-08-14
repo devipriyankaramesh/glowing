71a27a40e87a99b56f85d0969fc40438d483edf6
Devi Priyanka <devi26priya@gmail.com>
2016-08-14 11:51:55 +0530

    Create arraysum.java

---
+++ arraysum.java
@@ -0,0 +1,24 @@
+import java.i0.*;
+import java.util.*;
+class arraysum
+{
+ public static void main(String[] args)
+ {
+  Scanner s=new Scanner(System.in);
+  int n=s.nextInt();
+  int[] a=new int[n];
+  for(int i=0;i<n;i++)
+      a[i]=s.nextInt();
+  for(int i=0;i<n;i++)
+  {
+   for(int j=i+1;j<n;j++)
+   {
+    for(int k=j+1;k<n;k++)
+    {
+     if(a[i]+a[j]==a[k])
+      {
+        System.out.println(a[i]+" "+a[j]+" "+a[k]);
+        break;
+      }
+    }
+  }
