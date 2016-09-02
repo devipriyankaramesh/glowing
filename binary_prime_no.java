b91a1848f65a2ef341e7eb454af5f1f183f4baad
Devi Priyanka <devi26priya@gmail.com>
2016-09-02 11:17:45 +0530

    Update binary_prime_no.java

--- binary_prime_no.java
+++ binary_prime_no.java
@@ -9,22 +9,25 @@ class binary_prime_no
   int count=cal_prime(a,b);
   System.out.println(count);
 }
-public static cal_prime(int m,int n)
+public static int cal_prime(int m,int n)
 {
+ int prime_no_count=0;
  for(int i=m;i<=n;i++)
  {
-  int j=0,count=0,prim_no_count=0,c=0;
-  String ss=Integer.toString(m,2);
+  int count=0,c=0;
+    String ss=Integer.toString(i,2); System.out.println(ss+"------"+i);
   for(int j=0;j<ss.length();j++)
     if(ss.charAt(j)=='1')
        count++;
+  System.out.println(count);
   for(int k=2;k<=count/2;k++)
   {
     if(count%k==0){  c=1;  }
   }
-  if(c==0)
-     prime_no_count++;
+  if(c==0){
+     prime_no_count++;}
+  
 }
 return prime_no_count;
-}
+	}
 }
