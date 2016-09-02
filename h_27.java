4c6ca4ef60629419c14ebd08130ca20f0c038357
Devi Priyanka <devi26priya@gmail.com>
2016-09-02 12:18:37 +0530

    Update h_27.java

--- h_27.java
+++ h_27.java
@@ -8,6 +8,12 @@ class h_27
   int max=0;
   for(int i=0;i<ss.length();i++)
   {
+   for(int j=i+1;j<=ss.length();j++)
+   {  System.out.println(ss.substring(i,j)+"........"+ss.substring(i,j).length());}}
+   if("abaca".equals("acaba")){ System.out.println("yes");}
+   else { System.out.println("no"); }
+  for(int i=0;i<ss.length();i++)
+  {
    for(int j=i+1;j<ss.length();j++)
    {
     String s1=ss.substring(i,j);
@@ -16,9 +22,10 @@ class h_27
     String s2=new String(sb.reverse());
     if(s1.equals(s2)){  continue;  }
     else{
-         if(max<maxxx){max=maxxx;}}
+         if(max<maxxx){   max=maxxx;  }  }
    }
   }
   System.out.println(max);
+	
 	
   }}
